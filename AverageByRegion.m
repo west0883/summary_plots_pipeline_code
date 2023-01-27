f1b599a05d13f7c878e805d3fb4fcdbb2478b28a
Sarah West <west0883@umn.edu>
2023-01-27 10:30:25 -0600

    streamiline and add null distribution option

--- AverageByRegion.m
+++ AverageByRegion.m
@@ -10,28 +10,41 @@ function [parameters] = AverageByRegion(parameters)
     comparison = parameters.values{find(strcmp(parameters.keywords, 'comparison'))};
     comparisoni = parameters.values{find(strcmp(parameters.keywords, 'comparison_iterator'))};
     regions = fieldnames(parameters.regions);
+   
     
-    data_out = NaN(1, numel(regions),2);
+    % If not on null distributions
+    if ~parameters.null_distribution_flag
+        data_out = NaN(numel(regions), 2);
+    
+        for regioni = 1: numel(regions)
+    
+            region = regions{regioni};
+    
+            % average inside region
+            vals_inside = mean(data(parameters.regions.(region), :),1);
+             
+            % average across mice
+            data_out(regioni, 1) = mean(vals_inside, 2);
+            data_out(regioni, 2) = std(vals_inside, [], 2); 
+        end
 
-    for regioni = 1: numel(regions)
+    % If on null distributions
+    else
+        data_out = NaN(numel(regions), 2, size(data, 3));
 
-        region = regions{regioni};
-
-        % average inside region
-        vals_inside = mean(data(parameters.regions.(region), :),1);
-         
-        
-        % average across mice
-
-        data_out(1, regioni, 1) = mean(vals_inside, 2);
-        data_out(1, regioni, 2) = std(vals_inside, [], 2); 
-        
-
-%         vals(comparisoni).(region).avg_across_mice = mean(vals_inside, 2);
-%         vals(comparisoni).(region).std_across_mice = std(vals_inside, [], 2); 
-%         vals(comparisoni).comparison = comparison;
-       
-
+        for regioni = 1: numel(regions)
+    
+            region = regions{regioni};
+    
+            % average inside region
+            vals_inside = mean(data(parameters.regions.(region), :, :), 1);
+             
+            % average across mice
+            data_out(regioni, 1, :) = mean(vals_inside, 2);
+            data_out(regioni, 2, :) = std(vals_inside, [], 2); 
+        end
     end
+    
+    parameters.data_out = data_out;
 
 end 
